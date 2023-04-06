0825ff538d7757732014a8bb2d0a173996d8520d
yasin <baschirouabdelkarim@gmail.com>
2023-04-06 11:14:44 +0300

    testing

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 async function getCourse() {
-    const response = await fetch("http:localhost:3000/sendCourse", { method: "GET" });
+    const response = await fetch("/sendCourse", { method: "GET" });
     const data = await response.json()
     // console.log(data)
     // populateCourseTable(data)
