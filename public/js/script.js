52b65188ace58097e2f702a41415dcd1483dbdb9
Khawlah Naeem <khawlahnaeem@gmail.com>
2023-04-10 14:43:30 +0300

    Removed password showing from user side

--- public/js/script.js
+++ public/js/script.js
@@ -1,84 +1,82 @@
 // const server = 'http://localhost:3000';
-userStudCourse = []
-userLecCourse = []
-userCourses = []
-allCourses = []
+userStudCourse = [];
+userLecCourse = [];
+userCourses = [];
+allCourses = [];
 
 async function getSession() {
-    let response = await fetch('/getSession', { method: "GET" })
-    let data = await response.text()
-    let sessionInfo = JSON.parse(data)
-    userStudCourse = sessionInfo.courses.student
-    userLecCourse = sessionInfo.courses.lecturer
-    userCourses = sessionInfo.courses
-    populateInfoTable(sessionInfo)
+  let response = await fetch("/getSession", { method: "GET" });
+  let data = await response.text();
+  let sessionInfo = JSON.parse(data);
+  userStudCourse = sessionInfo.courses.student;
+  userLecCourse = sessionInfo.courses.lecturer;
+  userCourses = sessionInfo.courses;
+  populateInfoTable(sessionInfo);
 }
-
 
 //Gets a list of all student and lecturers
 async function getCourse() {
-    let response = await fetch("/sendCourse", { method: "GET" });
-    let data = await response.text();
-    allCourses = JSON.parse(data);
-    populateCourseTable(allCourses)
-
+  let response = await fetch("/sendCourse", { method: "GET" });
+  let data = await response.text();
+  allCourses = JSON.parse(data);
+  populateCourseTable(allCourses);
 }
 
 //Populate the table with the user's informations
 function populateInfoTable(information) {
-    let table = document.getElementById('informationTable');
-    var row = document.createElement('tr');
-    var dataId = document.createElement('td');
-    var textId = document.createTextNode(information.id);
-    dataId.appendChild(textId);
-    var dataName = document.createElement('td');
-    var textName = document.createTextNode(information.stud_name);
-    dataName.appendChild(textName);
-    var dataPass = document.createElement('td');
-    var textPass = document.createTextNode(information.Password);
-    dataPass.appendChild(textPass);
-    var dataEmail = document.createElement('td');
-    var textEmail = document.createTextNode(information.email);
-    dataEmail.appendChild(textEmail);
-    row.appendChild(dataId)
-    row.appendChild(dataName)
-    row.appendChild(textPass)
-    row.appendChild(dataEmail)
-    table.appendChild(row)
-    getCourse()
+  let table = document.getElementById("informationTable");
+  var row = document.createElement("tr");
+  var dataId = document.createElement("td");
+  var textId = document.createTextNode(information.id);
+  dataId.appendChild(textId);
+  var dataName = document.createElement("td");
+  var textName = document.createTextNode(information.stud_name);
+  dataName.appendChild(textName);
+  var dataPass = document.createElement("td");
+  var textPass = document.createTextNode(information.Password);
+  dataPass.appendChild(textPass);
+  var dataEmail = document.createElement("td");
+  var textEmail = document.createTextNode(information.email);
+  dataEmail.appendChild(textEmail);
+  row.appendChild(dataId);
+  row.appendChild(dataName);
+  //row.appendChild(textPass);
+  row.appendChild(dataEmail);
+  table.appendChild(row);
+  getCourse();
 }
 
 //Populate the course Information table
 function populateCourseTable(courses) {
-    courses.forEach(course => {
-        if (userStudCourse.includes(course.id)) {
-            createRow(course, 'Student')
-        }
-        if (userLecCourse.includes(course.id)) {
-            createRow(course, 'Lecturer')
-        }
-    });
+  courses.forEach((course) => {
+    if (userStudCourse.includes(course.id)) {
+      createRow(course, "Student");
+    }
+    if (userLecCourse.includes(course.id)) {
+      createRow(course, "Lecturer");
+    }
+  });
 }
 
 // Function that creates the row with the type of individual i.e student or lecturer
 function createRow(course, type) {
-    var table = document.getElementById('courseTable');
-    var row = document.createElement('tr');
-    var dataId = document.createElement('td');
-    var textId = document.createTextNode(course.id);
-    dataId.appendChild(textId)
-    var dataName = document.createElement('td');
-    var textName = document.createTextNode(course.c_name);
-    dataName.appendChild(textName);
-    var dataCourse = document.createElement('td');
-    var textCourse = document.createTextNode(course.description);
-    dataCourse.appendChild(textCourse)
-    var dataType = document.createElement('td');
-    var textType = document.createTextNode(type);
-    dataType.appendChild(textType);
-    row.appendChild(dataId)
-    row.appendChild(dataName)
-    row.appendChild(dataCourse)
-    row.appendChild(dataType)
-    table.appendChild(row);
+  var table = document.getElementById("courseTable");
+  var row = document.createElement("tr");
+  var dataId = document.createElement("td");
+  var textId = document.createTextNode(course.id);
+  dataId.appendChild(textId);
+  var dataName = document.createElement("td");
+  var textName = document.createTextNode(course.c_name);
+  dataName.appendChild(textName);
+  var dataCourse = document.createElement("td");
+  var textCourse = document.createTextNode(course.description);
+  dataCourse.appendChild(textCourse);
+  var dataType = document.createElement("td");
+  var textType = document.createTextNode(type);
+  dataType.appendChild(textType);
+  row.appendChild(dataId);
+  row.appendChild(dataName);
+  row.appendChild(dataCourse);
+  row.appendChild(dataType);
+  table.appendChild(row);
 }
